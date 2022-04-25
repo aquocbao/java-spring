1e898894e874f9abe5eb4f8e2f942780de21e27c
bpham <aquocbao1340@gmail.com>
2022-04-25 14:38:07 +0700

    update unit test for user

--- bp-spring-project-sample/src/test/java/com/example/bp/UsersTest.java
+++ bp-spring-project-sample/src/test/java/com/example/bp/UsersTest.java
@@ -3,17 +3,16 @@
  */
 package com.example.bp;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import com.example.bp.common.constant.BpResponse;
-import com.example.bp.model.entity.User;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.bp.model.dto.AssignedUserRoleDTO;
+import com.example.bp.model.dto.UserDTO;
+import com.example.bp.model.dto.UserRoleDTO;
+import com.example.bp.service.UserService;
 
 /**
  * @author bao.pham
@@ -23,16 +22,41 @@ public class UsersTest extends AbstractRestApiTest {
 
   private static final String REST_URL = "/users";
 
+  @Autowired
+  UserService userService;
+
   @Test
-  public void testRead() throws JsonMappingException, JsonProcessingException {
-    HttpHeaders header = new HttpHeaders();
-    header.add("Content-Type", "application/json");
-    HttpEntity<User> entity = new HttpEntity<User>(header);
-    ResponseEntity<BpResponse> response =
-        this.restTemplate.exchange(REST_URL + "/1", HttpMethod.GET, entity, BpResponse.class);
-    assertEquals(response.getStatusCode(), HttpStatus.OK);
-
-
+  public void testReadAll() throws Exception {
+    List<UserDTO> user = userService.findAll();
+    assertTrue(!user.isEmpty());
   }
+  
+  @Test
+  public void testReadById() throws Exception {
+    UserDTO user = userService.findById(1L);
+    assertEquals(user.getUsername(), "username01");
+  }
+  
+  @Test
+  public void testCreate() throws Exception {
+    UserDTO entity = new UserDTO();
+    entity.setUsername("username04");
+    entity.setFirstName("Pham");
+    entity.setLastName("B");
+    entity.setPassword("123456");
+    AssignedUserRoleDTO assignedUserRoleDTO = new AssignedUserRoleDTO();
+    UserRoleDTO role = new UserRoleDTO();
+    new UserRoleDTO().setUserRoleId(1L);;
+    assignedUserRoleDTO.setUserRole(role);
+    UserDTO user = userService.save(entity);
+    assertEquals(user.getUsername(), "username04");
+  }
+  
+  @Test
+  public void testReadByUsername() throws Exception {
+    UserDTO user = userService.findByUserName("username03");
+    assertEquals(user.getUsername(), "username03");
+  }
+
 
 }
